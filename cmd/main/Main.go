948c1df0048c318416abba812542af5f69174d79
nicole <Nicole@LAPTOP-G4JN8BP2>
2022-05-13 18:24:35 +0300

    Connect to DB on server start

--- cmd/main/Main.go
+++ cmd/main/Main.go
@@ -7,16 +7,17 @@ import (
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
+	"github.com/nic_pan/skincare-ingredients/pkg/config"
 	"github.com/nic_pan/skincare-ingredients/pkg/routes"
 )
 
 func main() {
 	startServer()
-
+	config.ConnectToDB()
 }
 
 func startServer() {
-	router := mux.NewRouter()
+	router := mux.NewRouter().StrictSlash(true)
 
 	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Welcome, skincare\n")
